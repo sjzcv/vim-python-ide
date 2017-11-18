c6d73560aa1be24cb07518ddbe6ba70c8d0670d7
Jarol Rodriguez <jarolsemail@gmail.com>
2017-11-18 10:13:31 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -6,7 +6,7 @@ wrapper() {
   BLUE="\033[0;36m"
   NORMAL="\033[0m"
 
-  REPO_HTTPS="https://github.com/jarolrod/python-vim-ide.git"
+  REPO_HTTPS="https://github.com/jarolrod/vim-python-ide.git"
   VUNDLE_HTTPS="https://github.com/VundleVim/Vundle.vim.git"
 
 echo "${BLUE}"
